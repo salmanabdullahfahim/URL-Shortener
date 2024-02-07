88306c6bf14df1b13a9b942ebca31ce73e6ad813
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-02-07 15:40:15 +0600

    added some middleware

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -10,9 +10,14 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
